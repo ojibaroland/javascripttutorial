ca23297ffabd676ed0fdd715e7d655efae740b7b
ojibaroland <112161397+ojibaroland@users.noreply.github.com>
2026-04-15 16:34:25 +0100

    update

--- variables/script.js
+++ variables/script.js
@@ -1,7 +1,61 @@
 // Creating variable using "let" keyword
-let name = "Chibuikem";
-console.log(name);
+// let name = "Chibuikem";
+// console.log(name);
 
 // console.log(name);
 
 // DATATYPES
+
+
+// Strings
+
+// const exampleString = "Hello World";
+const name = "Jane";
+const backticks = `${2 + 2}`;
+// console.log(typeof backticks);
+
+// Numbers
+
+const firstNumber = 'Hello';
+const secondNumber = 10;
+
+const result = firstNumber / secondNumber;
+
+// console.log(typeof result);
+
+// Booleans
+
+// true
+// false
+
+const isCool = false;
+
+// console.log(typeof isCool);
+
+if(isCool) {
+    console.log("Hi man, you're cool")
+} else {
+    console.log('Oh, hi...')
+};
+
+// const age = 20;
+// console.log(age < 18);
+
+//Null
+let age = null;
+
+
+
+// console.log(typeof age);
+
+// Undefined
+
+let x;
+
+// console.log(typeof x);
+
+// Objects
+
+const name = 'John';
+const age = 25;
+
