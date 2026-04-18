975242ab7818a48812a6aeb4f514fff12d5c2a1e
ojibaroland <112161397+ojibaroland@users.noreply.github.com>
2026-04-18 09:36:49 +0100

    update

--- variables/script.js
+++ variables/script.js
@@ -11,15 +11,15 @@
 
 // const exampleString = "Hello World";
 // const name = "Jane";
-const backticks = `${2 + 2}`;
-// console.log(typeof backticks);
+// const backticks = `${2 + 2}`;
+// // console.log(typeof backticks);
 
-// Numbers
+// // Numbers
 
-const firstNumber = 'Hello';
-const secondNumber = 10;
+// const firstNumber = 'Hello';
+// const secondNumber = 10;
 
-const result = firstNumber / secondNumber;
+// const result = firstNumber / secondNumber;
 
 // console.log(typeof result);
 
@@ -28,36 +28,75 @@ const result = firstNumber / secondNumber;
 // true
 // false
 
-const isCool = false;
+// const isCool = false;
 
-// console.log(typeof isCool);
+// // console.log(typeof isCool);
 
-if(isCool) {
-    console.log("Hi man, you're cool")
-} else {
-    console.log('Oh, hi...')
-};
+// if(isCool) {
+//     console.log("Hi man, you're cool")
+// } else {
+//     console.log('Oh, hi...')
+// };
 
-// const age = 20;
-// console.log(age < 18);
+// // const age = 20;
+// // console.log(age < 18);
 
-//Null
-// let age = null;
+// //Null
+// // let age = null;
 
 
 
-// console.log(typeof age);
+// // console.log(typeof age);
 
-// Undefined
+// // Undefined
 
-let x;
+// let x;
 
-// console.log(typeof x);
+// // console.log(typeof x);
 
-// Objects
+// // Objects
 
-const name = 'John';
-const age = 25;
+// // const name = 'John';
+// // const age = 25;
 
-const firstName  = "Chibuikem"
-const lastName = "Ojiba"
+// const person = {
+//     name: 'John',
+//     age: 25,
+
+// };
+
+// // Dot Notation
+
+// console.log(typeof person);
+
+// // Array
+
+// const arr = [1,2,3,4];
+// console.log(typeof arr);
+
+// const date = new Date();
+
+// console.log(typeof date);
+
+// TYPES OF PROGRAMMING
+
+// Statically__typed
+// let message = 'Hello World';
+
+// console.log(message)
+// console.log(typeof message)
+
+
+// message = 5;
+
+// console.log(message)
+// console.log(typeof message)
+// Dynamically__typed
+
+// Javascript is dynamically typed
+
+
+// Comparison Operator => true/false
+
+const a = 5;
+const b = 10;
